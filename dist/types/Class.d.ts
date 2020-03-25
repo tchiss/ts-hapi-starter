02ee522eb756270e09aef2fb63545ded273b1970
Nestor <tchiss_a@epitech.eu>
2020-03-25 16:41:08 +0100

    feat(dist): removing dist directory

--- dist/types/Class.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class MyClass {
-    props: string;
-    constructor(str: string);
-}
--- dist/types/common/Logger.d.ts
+++
@@ -1,3 +0,0 @@
-import * as Winston from 'winston';
-declare const _default: Winston.Logger;
-export default _default;
